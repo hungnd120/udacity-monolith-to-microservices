70e7c4b7077ec56df3dd5d38cb17d12e1caf1356
hungnd120 <knight.elite92@gmail.com>
2024-09-19 15:30:43 +0700

    test

--- udagram-api/src/controllers/v0/feed/routes/feed.router.ts
+++ udagram-api/src/controllers/v0/feed/routes/feed.router.ts
@@ -19,7 +19,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
 
   const token = tokenBearer[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
-    if (err) {
+      if (err) {
+        console.log('error')
       return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
     }
     return next();
